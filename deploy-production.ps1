ed03deba8da0f523dc9d35ed94b233a93efaf2ce
Dan Dickout <dandickout@hotmail.com>
2024-09-07 17:12:06 -0400

    added bootstrap instruction in deployment scripts

--- deploy-production.ps1
+++ deploy-production.ps1
@@ -1,2 +1,4 @@
 #this deployment script is for Windows OS
+#bootstrap the project the first time
+#npx cdk bootstrap --app "npx ts-node bin/production-your-app-name-cdk-be.ts" --profile your-profile-name
 npx cdk deploy --app "ts-node bin/production-your-app-name-cdk-be.ts" --profile your-profile-name
